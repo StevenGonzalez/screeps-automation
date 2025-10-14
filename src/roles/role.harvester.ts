862fbd4fd9d461bbc2a0d459483722b8516f7510
Steven Gonzalez <gonzalez.steven.m@gmail.com>
2025-10-14 00:41:36 -0500

    Improve upgrader and harvestor logic

--- src/roles/role.harvester.ts
+++ src/roles/role.harvester.ts
@@ -1,19 +1,41 @@
 import {
-  getClosestSpawn,
   getSources,
   harvestFromSource,
   isCreepFull,
+  isCreepEmpty,
+  findEnergyDepositTarget,
   transferEnergyTo,
 } from "../services/services.creep";
+import { ROLE_HARVESTER } from "../config/config.roles";
 
 export function runHarvester(creep: Creep) {
-  if (isCreepFull(creep)) {
-    const spawn = getClosestSpawn(creep.room, creep.pos);
-    if (spawn) {
-      transferEnergyTo(creep, spawn);
+  if (creep.memory.working === undefined) creep.memory.working = false;
+
+  if (creep.memory.working && isCreepEmpty(creep)) {
+    creep.memory.working = false;
+  }
+
+  if (!creep.memory.working && isCreepFull(creep)) {
+    creep.memory.working = true;
+  }
+
+  if (creep.memory.working) {
+    const depositTarget = findEnergyDepositTarget(creep, ROLE_HARVESTER);
+    if (depositTarget) {
+      transferEnergyTo(creep, depositTarget);
+    } else {
+      if (creep.room.controller) {
+        if (
+          creep.upgradeController(creep.room.controller) === ERR_NOT_IN_RANGE
+        ) {
+          creep.moveTo(creep.room.controller);
+        }
+      }
     }
   } else {
     const sources = getSources(creep.room);
-    harvestFromSource(creep, sources[0]);
+    if (sources.length > 0) {
+      harvestFromSource(creep, sources[0]);
+    }
   }
 }
--- src/roles/role.upgrader.ts
+++ src/roles/role.upgrader.ts
@@ -1,11 +1,22 @@
 import {
   getSources,
+  isCreepEmpty,
   isCreepFull,
   harvestFromSource,
 } from "../services/services.creep";
 
 export function runUpgrader(creep: Creep) {
-  if (isCreepFull(creep)) {
+  if (creep.memory.working === undefined) creep.memory.working = false;
+
+  if (creep.memory.working && isCreepEmpty(creep)) {
+    creep.memory.working = false;
+  }
+
+  if (!creep.memory.working && isCreepFull(creep)) {
+    creep.memory.working = true;
+  }
+
+  if (creep.memory.working) {
     if (creep.upgradeController(creep.room.controller!) === ERR_NOT_IN_RANGE) {
       creep.moveTo(creep.room.controller!);
     }
