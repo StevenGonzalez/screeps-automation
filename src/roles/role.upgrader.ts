c8ab5ac67156690c94b391fd12a54a4da93d46c4
Steven Gonzalez <gonzalez.steven.m@gmail.com>
2025-11-01 02:37:36 -0500

    Improve logic for where upgraders pull energy from

--- src/roles/role.upgrader.ts
+++ src/roles/role.upgrader.ts
@@ -5,8 +5,8 @@ import {
   harvestFromSource,
   upgradeController,
   withdrawFromControllerContainer,
-  getMinerContainerIds,
   withdrawFromContainer,
+  findClosestMinerContainerWithEnergy,
 } from "../services/services.creep";
 
 export function runUpgrader(creep: Creep) {
@@ -40,6 +40,21 @@ export function runUpgrader(creep: Creep) {
     }
 
     if (withdrawFromControllerContainer(creep)) return;
+
+    const closestMinerContainer = findClosestMinerContainerWithEnergy(creep);
+    if (closestMinerContainer) {
+      if (withdrawFromContainer(creep, closestMinerContainer)) return;
+    }
+
+    const storage = creep.room.storage;
+    if (storage && storage.store && storage.store[RESOURCE_ENERGY] > 0) {
+      const res = creep.withdraw(storage, RESOURCE_ENERGY);
+      if (res === ERR_NOT_IN_RANGE) {
+        creep.moveTo(storage);
+        return;
+      }
+      if (res === OK) return;
+    }
 
     const sources = getSources(creep.room);
     if (sources.length > 0) {
